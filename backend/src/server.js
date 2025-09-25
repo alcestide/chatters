e573ffaa0e36bd3cdde0a53388bd9d9440ca50b6
Angelo <angpanariti@gmail.com>
2025-09-25 14:24:26 +0200

    FE/BE boilerplates, basic routing (auth, message)

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+
+dotenv.config();
+
+const app = express();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}!`)
+}
+);
+
+
